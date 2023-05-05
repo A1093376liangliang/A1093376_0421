08a50a4dccbc8be41968fd678fdd1acd12ef4dfb
A1093376liangliang <125643622+A1093376liangliang@users.noreply.github.com>
2023-05-05 13:52:43 +0800

    Update A1093376_0421_1.java

--- A1093376_0421_1.java
+++ A1093376_0421_1.java
@@ -6,7 +6,6 @@ public class A1093376_0421_1{
         System.out.print("請輸入電子郵件信箱(小寫):");
         Scanner input = new Scanner(System.in);
         x=input.nextLine();
-        //System.out.println(x.matches("[a-z0-9]*[@]{1}[m]{1}[a]{1}[i]{1}[l]{1}[.]{1}[c]{1}[o]{1}[m]{1}"));
         if(x.matches("[a-z0-9]*[@]{1}[m]{1}[a]{1}[i]{1}[l]{1}[.]{1}[c]{1}[o]{1}[m]{1}") == true){
             System.out.println("輸入格式正確");
         }else{
