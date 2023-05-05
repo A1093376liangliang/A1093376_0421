d20efac2f21d3a782f317e9ebf3f4dc2c1e6543c
A1093376liangliang <125643622+A1093376liangliang@users.noreply.github.com>
2023-05-05 13:53:40 +0800

    Update A1093376_0421_2.java

--- A1093376_0421_2.java
+++ A1093376_0421_2.java
@@ -6,8 +6,6 @@ public class A1093376_0421_2{
         System.out.print("請輸入日期(YYYY/MM/DD或MM/DD/YYYY):");
         Scanner input = new Scanner(System.in);
         x=input.nextLine();
-        //System.out.println(x.matches("[0-9]{4}[/]{1}[0-1]{1}[0-9]{1}[/]{1}[0-3]{1}[0-9]{1}"));
-        //System.out.println(x.matches("[0-1]{1}[0-9]{1}[/]{1}[0-3]{1}[0-9]{1}[/]{1}[0-9]{4}"));
         if(x.matches("[0-9]{4}[/]{1}[0-1]{1}[0-9]{1}[/]{1}[0-3]{1}[0-9]{1}") == true || x.matches("[0-1]{1}[0-9]{1}[/]{1}[0-3]{1}[0-9]{1}[/]{1}[0-9]{4}") == true){
             System.out.println("輸入格式正確");
         }else{
